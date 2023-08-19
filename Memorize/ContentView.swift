d4e71b2edc9edc5b2b9d81638fd696d0268c0e81
Mike Neal <mike@bluepixel.dev>
2023-08-19 00:34:35 -0400

    update views

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var body: some View {
+		HStack {
+			CardView(isFaceUp: true)
+			CardView()
+			CardView()
+			CardView()
+		}
+		.foregroundColor(.orange)
+		.padding()
+
+	}
+}
+
+struct CardView : View {
+	var isFaceUp: Bool = false
+	
+	var body: some View {
+		ZStack {
+			if isFaceUp {
+				RoundedRectangle(cornerRadius: 12)
+					.foregroundColor(.white)
+				RoundedRectangle(cornerRadius: 12)
+					.strokeBorder(lineWidth: 2)
+				Text("👻")
+			} else {
+				RoundedRectangle(cornerRadius: 12)
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
